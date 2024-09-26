d88966dbf3adefe3ae47ca00669b9a78e4f83035
Tess Thomas <104388884+tessthom@users.noreply.github.com>
2024-09-26 11:44:40 -0400

    Add font-awesome, bulma CDN, and additional layout HTML

--- views/admin/layout.js
+++ views/admin/layout.js
@@ -6,9 +6,32 @@ export default ({ title, content }) => { // gets passed an object with the page-
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>${title}</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" rel="stylesheet" />
+        <link href="/css/main.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.2/css/bulma.min.css" integrity="sha512-RpeJZX3aH5oZN3U3JhE7Sd+HG8XQsqmP3clIbu4G28p668yNsRNj3zMASKe1ATjl/W80wuEtCx2dFA8xaebG5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       </head>
-      <body>
-        ${content}
+      <body class="admin">
+        <header>
+          <nav class="navbar navbar-bottom">
+            <div class="container navbar-container">
+              <div>
+                <a href="/admin/products">
+                  <h3 class="title">Admin Panel</h3>
+                </a>
+              </div>
+              <div class="navbar-item">
+                <div class="navbar-buttons">
+                  <div class="navbar-item">
+                    <a href="/admin/products"><i class="fa fa-star"></i> Products</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </header>
+        <div class="container">
+          ${content}
+        </div>
       </body>
     </html>
   `;
